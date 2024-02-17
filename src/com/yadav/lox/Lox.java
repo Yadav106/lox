b3670c9d7457090375590315becc441fa3bf62cf
Yadav106 <sahil.jp.yadav@gmail.com>
2024-02-17 17:01:19 +0530

    added parsing logic to the interpreter

--- src/com/yadav/lox/Lox.java
+++ src/com/yadav/lox/Lox.java
@@ -45,11 +45,15 @@ public class Lox {
         Scanner scanner = new Scanner(source);
 
         List<Token> tokens = scanner.scanTokens();
-
-        // For now, just print the tokens.
-        for (Token token : tokens) {
-            System.out.println(token);
-        }
+        
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+        
+        // Stop if there was a syntax error
+        if (hadError) return;
+        
+        System.out.println(new AstPrinter().print(expression));
+        
     }
 
     static void error(int line, String message) {
@@ -61,5 +65,12 @@ public class Lox {
                 "[line " + line + "] Error" + where + ": " + message
         );
         hadError = true;
+    }
+    static void error(Token token, String message) {
+      if (token.type == TokenType.EOF) {
+        report(token.line, " at end", message);
+      } else {
+        report(token.line, " at " + token.lexeme + "'", message);
+      }
     }
 }
---
+++ src/com/yadav/lox/Parser.java
@@ -0,0 +1,169 @@
+package com.yadav.lox;
+
+import java.util.List;
+
+class Parser {
+  private static class ParseError extends RuntimeException {}
+
+  // class fields
+  private final List<Token> tokens;
+  private int current = 0;
+
+  // Constructor
+  Parser(List<Token> tokens) { this.tokens = tokens; }
+
+  Expr parse() {
+    try {
+      return expression();
+    } catch (ParseError error) {
+      return null;
+    }
+  }
+
+  // utility functions
+  private boolean match(TokenType... types) {
+    for (TokenType type : types) {
+      if (check(type)) {
+        advance();
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private boolean check(TokenType type) {
+    if (isAtEnd())
+      return false;
+    return peek().type == type;
+  }
+
+  private Token advance() {
+    if (!isAtEnd())
+      current++;
+    return previous();
+  }
+
+  private boolean isAtEnd() { return peek().type == TokenType.EOF; }
+
+  private Token peek() { return tokens.get(current); }
+
+  private Token previous() { return tokens.get(current - 1); }
+
+  private Token consume(TokenType type, String message) {
+    if (check(type))
+      return advance();
+
+    throw error(peek(), message);
+  }
+
+  private ParseError error(Token token, String message) {
+    Lox.error(token, message);
+    return new ParseError();
+  }
+
+  private void synchronize() {
+    advance();
+
+    while (!isAtEnd()) {
+      if (previous().type == TokenType.SEMICOLON)
+        return;
+
+      switch (peek().type) {
+        case TokenType.CLASS:
+        case TokenType.FUN:
+        case TokenType.VAR:
+        case TokenType.FOR:
+        case TokenType.IF:
+        case TokenType.WHILE:
+        case TokenType.PRINT:
+        case TokenType.RETURN:
+          return;
+        default:
+      }
+
+      advance();
+    }
+  }
+
+  // rule functions
+  private Expr expression() { return equality(); }
+
+  private Expr equality() {
+    Expr expr = comparison();
+
+    while (match(TokenType.BANG_EQUAL, TokenType.EQUAL_EQUAL)) {
+      Token operator = previous();
+      Expr right = comparison();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  private Expr comparison() {
+    Expr expr = term();
+
+    while (match(TokenType.GREATER, TokenType.GREATER_EQUAL, TokenType.LESS,
+                 TokenType.LESS_EQUAL)) {
+      Token operator = previous();
+      Expr right = term();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  private Expr term() {
+    Expr expr = factor();
+
+    while (match(TokenType.PLUS, TokenType.MINUS)) {
+      Token operator = previous();
+      Expr right = factor();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  private Expr factor() {
+    Expr expr = unary();
+
+    while (match(TokenType.STAR, TokenType.SLASH)) {
+      Token operator = previous();
+      Expr right = unary();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  private Expr unary() {
+    if (match(TokenType.BANG, TokenType.MINUS)) {
+      Token operator = previous();
+      Expr right = unary();
+      return new Expr.Unary(operator, right);
+    }
+    return primary();
+  }
+
+  private Expr primary() {
+    if (match(TokenType.FALSE))
+      return new Expr.Literal(false);
+    if (match(TokenType.TRUE))
+      return new Expr.Literal(true);
+    if (match(TokenType.NIL))
+      return new Expr.Literal(null);
+
+    if (match(TokenType.NUMBER, TokenType.STRING)) {
+      return new Expr.Literal(previous().literal);
+    }
+
+    if (match(TokenType.LEFT_PAREN)) {
+      Expr expr = expression();
+      consume(TokenType.RIGHT_PAREN, "Expect ')' after expression.");
+      return new Expr.Grouping(expr);
+    }
+
+    throw error(peek(), "Expect expression.");
+  }
+}
