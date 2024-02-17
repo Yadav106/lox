b1474b4ce11dce8ae906c097cc345a9b48c13893
Yadav106 <sahil.jp.yadav@gmail.com>
2024-02-17 10:03:44 +0530

    added AstPrinter and Expr files, setup for parsing

---
+++ src/com/yadav/lox/AstPrinter.java
@@ -0,0 +1,59 @@
+package com.yadav.lox;
+
+import com.yadav.lox.Expr.Binary;
+import com.yadav.lox.Expr.Grouping;
+import com.yadav.lox.Expr.Literal;
+import com.yadav.lox.Expr.Unary;
+
+class AstPrinter implements Expr.Visitor<String> {
+  String print(Expr expr) {
+    return expr.accept(this);
+  }
+
+  @Override
+  public String visitBinaryExpr(Binary expr) {
+    return parenthesize(expr.operator.lexeme, expr.left, expr.right);
+  }
+
+  @Override
+  public String visitGroupingExpr(Grouping expr) {
+    return parenthesize("group", expr.expression);
+  }
+
+  @Override
+  public String visitLiteralExpr(Literal expr) {
+    if (expr.value == null) return "nil";
+    return expr.value.toString();
+  }
+
+  @Override
+  public String visitUnaryExpr(Unary expr) {
+    return parenthesize(expr.operator.lexeme, expr.right);
+  }
+
+  private String parenthesize(String name, Expr... exprs) {
+    StringBuilder builder = new StringBuilder();
+
+    builder.append("(").append(name);
+    for (Expr expr : exprs) {
+      builder.append(" ");
+      builder.append(expr.accept(this));
+    }
+    builder.append(")");
+
+    return builder.toString();
+  }
+
+  public static void main(String[] args) {
+    Expr expression = new Expr.Binary(
+        new Expr.Unary(
+          new Token(TokenType.MINUS, "-", null, 1),
+          new Expr.Literal(123)
+        ),
+        new Token(TokenType.STAR, "*", null, 1),
+        new Expr.Grouping(new Expr.Literal(45.55))
+      );
+
+    System.out.println(new AstPrinter().print(expression));
+  }
+}
