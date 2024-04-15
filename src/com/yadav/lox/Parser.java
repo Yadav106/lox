0d4f40a5582a306900d0b104772333804426a112
Yadav106 <sahil.jp.yadav@gmail.com>
2024-04-15 18:49:35 +0530

    added for loop

--- src/com/yadav/lox/Parser.java
+++ src/com/yadav/lox/Parser.java
@@ -1,6 +1,7 @@
 package com.yadav.lox;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static com.yadav.lox.TokenType.*;
@@ -55,12 +56,54 @@ class Parser {
   }
 
   private Stmt statement() {
+    if (match(FOR)) return forStatement();
     if (match(IF)) return ifStatement();
     if (match(PRINT)) return printStatement();
     if (match(WHILE)) return whileStatement();
     if (match(LEFT_BRACE)) return new Stmt.Block(block());
 
     return expressionStatement();
+  }
+
+  private Stmt forStatement() {
+    consume(LEFT_PAREN, "Expected '(' after 'for'.");
+
+    Stmt initializer;
+    if (match(SEMICOLON)) {
+      initializer = null;
+    } else if (match(VAR)) {
+      initializer = variableDeclaration();
+    } else {
+      initializer = expressionStatement();
+    }
+
+    Expr condition = null;
+    if (!check(SEMICOLON)) {
+      condition = expression();
+    }
+    consume(SEMICOLON, "Expected ';' after for loop condition");
+
+    Expr increment = null;
+    if (!check(SEMICOLON)) {
+      increment = expression();
+    }
+    consume(RIGHT_PAREN, "Expcted ')' after for clauses.");
+    Stmt body = statement();
+
+    if (increment != null) {
+      body = new Stmt.Block(Arrays.asList(
+        body,
+        new Stmt.Expression(increment)
+      ));
+    }
+
+    if (condition == null) condition = new Expr.Literal(true);
+    body = new Stmt.While(condition, body);
+
+    if (initializer != null) {
+      body = new Stmt.Block(Arrays.asList(initializer, body));
+    }
+    return body;
   }
 
   private Stmt whileStatement() {
