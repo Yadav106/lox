e46c7786e0d832359651be40631297a68cb4c5ad
Yadav106 <sahil.jp.yadav@gmail.com>
2024-04-15 13:41:13 +0530

    added if-else statements

--- src/com/yadav/lox/Parser.java
+++ src/com/yadav/lox/Parser.java
@@ -55,10 +55,26 @@ class Parser {
   }
 
   private Stmt statement() {
+    if (match(IF)) return ifStatement();
     if (match(PRINT)) return printStatement();
     if (match(LEFT_BRACE)) return new Stmt.Block(block());
 
     return expressionStatement();
+  }
+
+  private Stmt ifStatement() {
+    consume(LEFT_PAREN, "Expected '(' after an 'if' statement.");
+    Expr condition = expression();
+    consume(RIGHT_PAREN, "Expected ')' after if condition");
+
+    Stmt thenBranch = statement();
+    Stmt elseBranch = null;
+
+    if (match(ELSE)) {
+      elseBranch = statement();
+    }
+
+    return new Stmt.If(condition, thenBranch, elseBranch);
   }
 
   private List<Stmt> block() {
